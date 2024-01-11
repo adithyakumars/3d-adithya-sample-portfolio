15480018a1640317604ef401746909a4536b936f
sadithyakumar <adithyakumars2002@gmail.com>
2024-01-11 11:47:52 +0530

    first commit

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
-import { BallCanvas } from './canvas';
-import { SectionWrapper } from '../hoc';
-import { technologies } from '../constants';
+import { BallCanvas } from "./canvas";
+import { fadeIn, textVariant } from "../utils/motion";
 
-const Tech = () => {
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+import { styles } from "../styles";
+
+const TechCard = ({ index, icon }) => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-      <div className='w-28 h-28' key={technology.name}> 
-        <BallCanvas icon={technology.icon} />
+    <Tilt className="w-28 h-28 pt-10">
+      <div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className="w-full green-pink-gradient p-[2px] rounded-full shadow-card select-none">
+        <div options={{ max: 45, scale: 1, speed: 450 }} className="bg-tertiary rounded-full py-5 flex justify-evenly items-center flex-col">
+          <img src={icon} className="w-16 h-16 object-contain" />
+        </div>
       </div>
-      ))}
-    </div>
+    </Tilt>
   )
 }
 
-export default SectionWrapper(Tech, "");
+const Tech = () => {
+  return (
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>My Tools</p>
+      <h2 className={styles.sectionHeadText}>Technologies</h2>
+    </motion.div>
+    <div className='flex flex-row flex-wrap justify-center gap-5'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <TechCard icon={technology.icon} name={technology.name} />
+        </div>
+      ))}
+    </div>
+    </>
+  )
+}
+
+export default SectionWrapper(Tech, '');
